3b7540ada08e2055797ede6b886bb6e1a2958c1b
Pigonhair <pigonhairtt@gmail.com>
2023-03-09 17:52:09 +0900

    apply gitignore

--- aviation-museum/bin/src/main/webapp/assets/js/pages/task-creat.init.js
+++
@@ -1,1 +0,0 @@
-flatpickr("#datepicker-basic"),flatpickr("#datepicker-datetime-start",{enableTime:!0,dateFormat:"m-d-Y H:i"}),flatpickr("#datepicker-datetime-end",{enableTime:!0,dateFormat:"m-d-Y H:i"});
